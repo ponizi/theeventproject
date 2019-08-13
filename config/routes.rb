b67ecfa6fff030a4ab57cba5d588ed7003406e46
ponizi <ponalexis@gmail.com>
2019-08-13 15:46:27 +0200

    Salut

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
  root 'events#index'
  resources :events
+ resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
